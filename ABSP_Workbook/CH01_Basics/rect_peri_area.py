3480ba94a60face271eab499e6ccd8f062c86681
Shubham Rawat <shubhomrawat708@gmail.com>
2026-04-14 18:37:04 +0530

    feat: complete final ch1 exercise - rect calculator

--- ABSP_Workbook/CH01_Basics/rect_peri_area.py
+++ ABSP_Workbook/CH01_Basics/rect_peri_area.py
@@ -1,8 +1,8 @@
-lenght = int(input("Enter the lenght: "))
+length = int(input("Enter the length: "))
 width = int(input("Enter the width: "))
 
-perimeter = 2 * (lenght + width)
-area = lenght * width
+perimeter = 2 * (length + width)
+area = length * width
 
 print(f'The perimeter of rectangle is: {perimeter}')
 print(f'The area of rectangle is: {area}')
