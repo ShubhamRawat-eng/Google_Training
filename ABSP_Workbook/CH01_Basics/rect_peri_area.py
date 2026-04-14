2a4c83229f707a5466dbd9d7a74ab173a29e4a04
Shubham Rawat <shubhomrawat708@gmail.com>
2026-04-14 18:28:41 +0530

    feat: add rectangle perimeter and area calculator

---
+++ ABSP_Workbook/CH01_Basics/rect_peri_area.py
@@ -0,0 +1,8 @@
+lenght = int(input("Enter the lenght: "))
+width = int(input("Enter the width: "))
+
+perimeter = 2 * (lenght + width)
+area = lenght * width
+
+print(f'The perimeter of rectangle is: {perimeter}')
+print(f'The area of rectangle is: {area}')
