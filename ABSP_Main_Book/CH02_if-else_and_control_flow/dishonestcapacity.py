3f62edc5167ea1e7aa277e3804ce7929f42c90e9
Shubham Rawat <shubhomrawat708@gmail.com>
2026-04-26 16:33:39 +0530

    feat: add code to check true storage

---
+++ ABSP_Main_Book/CH02_if-else_and_control_flow/dishonestcapacity.py
@@ -0,0 +1,15 @@
+unit = input("Enter TB or GB for the advertised unit: ")
+
+#Calculate the amount that the advertised capacity lies:
+if unit == "TB" or unit == "tb":
+	discrepancy = 1000000000000 / 1099511627776
+elif unit == "GB" or unit == "gb":
+	discrepancy = 1000000000 / 1073741824
+ 
+advertised_capacity = float(input("Enter the advertised capacity: "))
+
+# Calculate the real capacity, round it to the nearest hunderdths,
+# and convert  it to a string so it cna be concatenated:
+real_capacity = str(round(advertised_capacity * discrepancy, 2))
+
+print(f"The actual capacity is {real_capacity} {unit}")
