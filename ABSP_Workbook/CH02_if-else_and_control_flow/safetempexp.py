e5ae080e83416559aace50e828403176d8b2d362
Shubham Rawat <shubhomrawat708@gmail.com>
2026-04-30 16:49:05 +0530

    feat: add consolidated logic version of temp checker

---
+++ ABSP_Workbook/CH02_if-else_and_control_flow/safetempexp.py
@@ -0,0 +1,7 @@
+scale = input("Enter C or F to indicate Celsius or Fahrenheit: ")
+degrees = float(input("Enter the number of the degrees: "))
+
+if (scale == 'C' and degrees >= 16 and degrees <= 38) or (scale == 'f' and degrees >= 60.8 and degrees <= 100.4):
+	print('Safe')
+else:
+	print('Dangerous')
