1d9723f7be01be39ea1b39fde11bb5a3100e1ff9
Shubham Rawat <shubhomrawat708@gmail.com>
2026-05-06 13:50:55 +0530

    feat: used for loop and made while loop version of for loop

---
+++ ABSP_Main_Book/CH03_loops/fiveTimes.py
@@ -0,0 +1,13 @@
+# with for loop
+print('Hello')
+for i in range(5):
+	print(f'On this iteration, i is set to {i}')
+print('Good bye')
+
+# with while loop
+print('Hello')
+i = 0 
+while i < 5:
+	print(f'On this iteration, i is set to {i} ')
+	i += 1
+print('Good bye')
