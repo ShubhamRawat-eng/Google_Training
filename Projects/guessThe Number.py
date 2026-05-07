8f61fd5e923e6450c3a5de3ce1da90ed991d824d
Shubham Rawat <shubhomrawat708@gmail.com>
2026-05-07 13:52:51 +0530

    refactor: moved games projects to dedicated projects folder

---
+++ Projects/guessThe Number.py
@@ -0,0 +1,21 @@
+# This is Guess the number game
+import random
+secret_number = random.randint(1,20)
+print('I am thinking of a number between 1 to 20.')
+
+# Ask the player to guess 6 times.
+for guesses_taken in range(1,7):
+	guess = int(input('Take a guess:'))
+
+	if guess < secret_number:
+		print('Your number is too low.')
+
+	elif guess > secret_number:
+		print('Your number is too High.')
+	else:
+		break
+
+if guess == secret_number:
+	print(f'Good job! You got it in {guesses_taken} guess!')
+else:
+	print(f"Nope. The number was {secret_number} ")
