a4c57fe479dfbe9ea34f7be25d5b59e433837c99
Shubham Rawat <shubhomrawat708@gmail.com>
2026-05-17 20:36:26 +0530

    feat: made a fortune ball game

---
+++ ABSP_Main_Book/CH04_functions/helloFunc2.py
@@ -0,0 +1,8 @@
+def say_hello_to(name):
+	# prints three greeting to the name provided
+	print(f'Good morning, {name}')
+	print(f'Good afternoon, {name}')
+	print(f'Good evening, {name}')
+
+say_hello_to('Alice')
+say_hello_to('Bob')
